d75a50d040c9cf9735a9659bcdbf84d482589751
p0rian <porianowski123@gmail.com>
2023-04-30 20:22:32 +0200

    update main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello <code>your computer</code> has a virus.
+          WHAT
         </p>
         <a
           className="App-link"
