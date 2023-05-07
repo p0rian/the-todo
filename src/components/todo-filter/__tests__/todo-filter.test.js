2e614b88cc01209962c327e0e0494226ed4f9a2a
p0rian <porianowski123@gmail.com>
2023-05-07 22:40:28 +0200

    working on todo item tests

--- src/components/todo-filter/__tests__/todo-filter.test.js
+++ src/components/todo-filter/__tests__/todo-filter.test.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { screen, fireEvent } from "@testing-library/react";
 import { renderWithProviders } from "utils/test-utils";
 import { TodoFilter } from "../todo-filter";
 
 describe("TodoFilter component", () => {
+  const filter = "all";
+  const handleFilterChange = jest.fn();
   it("renders correctly", () => {
-    const filter = "all";
-    const handleFilterChange = jest.fn();
     const { container } = renderWithProviders(
       <TodoFilter filter={filter} handleFilterChange={handleFilterChange} />,
     );
     expect(container).toMatchSnapshot();
+  });
+  it("renders all filter button", () => {
+    renderWithProviders(<TodoFilter filter={filter} handleFilterChange={handleFilterChange} />);
+    const buttonElement = screen.getByTestId("toggle-button-all");
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("renders active filter button", () => {
+    renderWithProviders(<TodoFilter filter={filter} handleFilterChange={handleFilterChange} />);
+    const buttonElement = screen.getByTestId("toggle-button-active");
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("renders completed filter button", () => {
+    renderWithProviders(<TodoFilter filter={filter} handleFilterChange={handleFilterChange} />);
+    const buttonElement = screen.getByTestId("toggle-button-completed");
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("clicking on filter button calls the handleFilterChange function", () => {
+    renderWithProviders(<TodoFilter filter={filter} handleFilterChange={handleFilterChange} />);
+    const activeFilterButton = screen.getByTestId("toggle-button-active");
+    fireEvent.click(activeFilterButton);
+    expect(handleFilterChange).toHaveBeenCalledWith(expect.any(Object), "active");
   });
 });
--- src/components/todo-item/__tests__/todo-item.test.js
+++ src/components/todo-item/__tests__/todo-item.test.js
@@ -1,15 +1,67 @@
 import React from "react";
 import { renderWithProviders } from "utils/test-utils";
+import { updateTodo } from "store/slices/todoSlice";
+import { useDispatch } from "react-redux";
+import { screen, fireEvent } from "@testing-library/react";
 import { TodoItem } from "../todo-item";
 
-describe("TodoItem component", () => {
+describe("TodoItem", () => {
+  const testItem = {
+    id: "testId",
+    title: "Test title",
+    isCompleted: false,
+  };
+
   it("renders correctly", () => {
-    const item = {
-      title: "Buy groceries",
-      isCompleted: false,
-      id: "1",
-    };
-    const { container } = renderWithProviders(<TodoItem item={item} />);
+    const { container } = renderWithProviders(<TodoItem item={testItem} />);
     expect(container).toMatchSnapshot();
   });
+  it("renders the item title and delete button", () => {
+    renderWithProviders(<TodoItem item={testItem} />);
+
+    expect(screen.getByText(testItem.title)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "X" })).toBeInTheDocument();
+  });
+
+  it("marks the item as completed when checkbox is checked", () => {
+    renderWithProviders(<TodoItem item={testItem} />);
+
+    fireEvent.click(screen.getByRole("checkbox"));
+    expect(screen.getByRole("checkbox")).toBeChecked();
+  });
+
+  // it("opens edit mode when clicking on the item title", () => {
+  //   renderWithProviders(<TodoItem item={testItem} />);
+
+  //   fireEvent.click(screen.getByText(testItem.title));
+  //   expect(screen.getByRole("textbox")).toBeInTheDocument();
+  //   expect(screen.getByRole("button", { name: "Save" })).toBeInTheDocument();
+  //   expect(screen.getByRole("button", { name: "Cancel" })).toBeInTheDocument();
+  // });
+
+  // it("updates the item title when 'Save' is clicked", () => {
+  //   const { store } = renderWithProviders(<TodoItem item={testItem} />);
+
+  //   fireEvent.click(screen.getByText(testItem.title));
+  //   fireEvent.change(screen.getByRole("textbox"), { target: { value: "New title" } });
+  //   fireEvent.click(screen.getByRole("button", { name: "Save" }));
+  //   expect(store.getState().todoData.entities[testItem.id].title).toBe("New title");
+  // });
+
+  // it("cancels edit mode when 'Cancel' is clicked", () => {
+  //   renderWithProviders(<TodoItem item={testItem} />);
+
+  //   fireEvent.click(screen.getByText(testItem.title));
+  //   fireEvent.change(screen.getByRole("textbox"), { target: { value: "New title" } });
+  //   fireEvent.click(screen.getByRole("button", { name: "Cancel" }));
+  //   expect(screen.queryByRole("textbox")).not.toBeInTheDocument();
+  //   expect(screen.getByText(testItem.title)).toBeInTheDocument();
+  // });
+
+  // it("deletes the item when 'X' is clicked", () => {
+  //   const { store } = renderWithProviders(<TodoItem item={testItem} />);
+
+  //   fireEvent.click(screen.getByRole("button", { name: "X" }));
+  //   expect(store.getState().todoData.entities[testItem.id]).toBeUndefined();
+  // });
 });
